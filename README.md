dbc06d92249f91dbd8a1bda11bfac306b7898870
Teetje Stark <teetje.stark@gmx.de>
2019-08-09 00:43:21 +0200

    Parameterize configuration and remove password requirement on certificate renewal

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Webserver written in Java for displaying Doppelkopf results (or any other 4 player game) stored in an external database.
 
 Doppelkopf-Server includes a REST API for communication with e.g. an app. New results can be added via this REST API or via the interface on a webpage.
+
+## Warning
+This server is not properly secured. It has known vulnerabilies. Only use it if you don't really care about the data you use.
--- renew.sh
+++ renew.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -eu
+
+source .env
 
 # update certificate if necessary
 certbot renew
 
 # save certificate chain and private key in keystore
-openssl pkcs12 -export -inkey /etc/letsencrypt/live/teetje-doko.de/privkey.pem -in /etc/letsencrypt/live/teetje-doko.de/fullchain.pem -out cert.p12
+openssl pkcs12 -export -inkey /etc/letsencrypt/live/teetje-doko.de/privkey.pem -in /etc/letsencrypt/live/teetje-doko.de/fullchain.pem -out cert.p12 -passout pass:${SSL_KEYSTORE_PASSWORD}
 
 # rebuild application
 docker-compose build
